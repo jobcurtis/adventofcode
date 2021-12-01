19876345edda02abf7b958ba7fd944a7f96cda3c
Job Curtis <job.curtis@vistair.com>
2021-12-01 09:40:06 +0000

    2021 day 1

---
+++ 2021/src/main/kotlin/Utils.kt
@@ -0,0 +1,12 @@
+package com.emlett.aoc
+
+import java.io.File
+import java.io.FileNotFoundException
+
+private val classloader = {}::class.java.classLoader!!
+
+private val String.fullPath: String
+    get() = classloader.getResource(this)?.path ?: throw FileNotFoundException()
+
+fun readAsLines(filename: String) = File(filename.fullPath).readLines()
+fun readAsInts(filename: String) = readAsLines(filename).map(String::toInt)
