773cca68e99321e5fc7dce9081ea6dd99e5e490c
Job Curtis <job.curtis@gmail.com>
2022-07-16 17:50:17 +0100

    2015 Day 5

---
+++ src/main/kotlin/y2015/Day05.kt
@@ -0,0 +1,13 @@
+package com.emlett.aoc.y2015
+
+object Day05 : Year2015() {
+    override fun part1() = lines.count { threeVowels(it) && doubleLetter(it) && noIllegal(it) }
+    override fun part2() = lines.count { repeatingPair(it) && symmetricalTriple(it) }
+
+    private fun noIllegal(string: String) = string.contains(Regex("(ab|cd|pq|xy)")).not()
+    private fun threeVowels(string: String) = string.count { it in charArrayOf('a', 'e', 'i', 'o', 'u') } >= 3
+    private fun doubleLetter(string: String) = string.windowed(2).any { it.first() == it.last() }
+
+    private fun repeatingPair(string: String) = string.contains(Regex("(..).*\\1"))
+    private fun symmetricalTriple(string: String) = string.windowed(3).any { it.first() == it.last() }
+}
