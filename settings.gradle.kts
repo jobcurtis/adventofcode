19a243efdd25d3e427d4c20e332d68407212a7e4
Job Curtis <job.curtis@gmail.com>
2022-11-30 18:23:59 +0000

    Update gradle to 7.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,5 @@
 rootProject.name = "adventofcode"
 
 plugins {
-    id("de.fayard.refreshVersions") version "0.40.2"
-}
-
-dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage") versionCatalogs {
-        create("libs") {
-            val kotlinVersion = version("kotlin", "1.7.10")
-
-            library("kotlin-test", "org.jetbrains.kotlin", "kotlin-test").versionRef(kotlinVersion)
-            library("kotlin-test-junit5", "org.jetbrains.kotlin", "kotlin-test-junit5").versionRef(kotlinVersion)
-
-            val coroutines = version("coroutines", "1.6.4")
-            library("kotlinx-coroutines", "org.jetbrains.kotlinx", "kotlinx-coroutines-core").versionRef(coroutines)
-
-            val kotestVersion = version("kotest", "5.4.2")
-            library("kotest-runner-junit5", "io.kotest", "kotest-runner-junit5").versionRef(kotestVersion)
-            library("kotest-assertions-core", "io.kotest", "kotest-assertions-core").versionRef(kotestVersion)
-            library("kotest-property", "io.kotest", "kotest-property").versionRef(kotestVersion)
-            library("kotest-datatest", "io.kotest", "kotest-framework-datatest").versionRef(kotestVersion)
-        }
-    }
+    id("de.fayard.refreshVersions") version "0.51.0"
 }
