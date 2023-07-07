b1152fd9fa9869dcfe13f5aedb07acf3157b6d5e
Job Curtis <job.curtis@gmail.com>
2023-07-07 11:32:00 +0100

    Update project configurations and dependencies
     - gradle 7.6 -> gradle 8.2
     - kotlin 1.7.22 -> kotlin 1.9.0
     - versions.properties -> libs.versions.toml
     - enable gradle build cache and configuration cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,7 @@ rootProject.name = "adventofcode"
 plugins {
     id("de.fayard.refreshVersions") version "0.51.0"
 }
+
+refreshVersions {
+    versionsPropertiesFile = file("build/tmp/versions.properties")
+}
