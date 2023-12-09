110060b823e96c47f501f9487dd78d2b70eda4c7
Job Curtis <job.curtis@gmail.com>
2023-12-09 10:09:01 +0000

    2023 Day 09

---
+++ src/main/kotlin/y2023/Day09.kt
@@ -0,0 +1,17 @@
+package com.emlett.aoc.y2023
+
+import com.emlett.aoc.utils.head
+import com.emlett.aoc.utils.input.integers
+import com.emlett.aoc.utils.tail
+
+object Day09 : Year2023() {
+    private val numbers by lazy { lines.map(String::integers) }
+
+    override fun part1() = numbers.sumOf { it.next() }
+    override fun part2() = numbers.sumOf { it.prev() }
+
+    private fun List<Int>.next(): Int = if (all(this[0]::equals)) this[0] else tail + differences().next()
+    private fun List<Int>.prev(): Int = if (all(this[0]::equals)) this[0] else head - differences().prev()
+
+    private fun List<Int>.differences() = windowed(2) { (a, b) -> b - a }
+}
