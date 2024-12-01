70c97c06b6b32ee2151f9544a051a21ba94e4955
Job Curtis <job.curtis@gmail.com>
2024-12-01 06:50:26 +0000

    2024 Day 01

---
+++ src/test/kotlin/Solutions2024.kt
@@ -0,0 +1,8 @@
+package com.emlett.aoc
+
+import com.emlett.aoc.y2024.Day01
+import org.junit.jupiter.api.Nested
+
+class Solutions2024 {
+    @Nested inner class Day01 : Solutions(Day01, 2970687, 23963899)
+}
