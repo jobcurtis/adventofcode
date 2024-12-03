86c652cfcd70c977d68ea8a1e9416da2ee9740d5
Job Curtis <job.curtis@gmail.com>
2024-12-03 09:59:32 +0000

    2024 Day 03

--- src/test/kotlin/Solutions2024.kt
+++ src/test/kotlin/Solutions2024.kt
@@ -6,4 +6,5 @@ import org.junit.jupiter.api.Nested
 class Solutions2024 {
     @Nested inner class Day01 : Solutions(Day01, 2970687, 23963899)
     @Nested inner class Day02 : Solutions(Day02, 306, 366)
+    @Nested inner class Day03 : Solutions(Day03, 187194524, 127092535)
 }
