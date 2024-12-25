30869172d908b08cfd43e71035194b0ac1238bfe
Job Curtis <job.curtis@gmail.com>
2024-12-25 09:41:34 +0000

    2024 Day 25

---
+++ src/main/kotlin/y2024/Day25.kt
@@ -0,0 +1,13 @@
+package com.emlett.aoc.y2024
+
+object Day25 : Year2024() {
+    val input by lazy { text.split("\n\n") }
+
+    override fun part1() = input.sumOf { a ->
+        input.count { b ->
+            a.zip(b).all { if (it.first == '#') it.second != '#' else true }
+        }
+    } / 2
+
+    override fun part2() = TODO()
+}
